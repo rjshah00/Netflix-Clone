f3e635549bf9d40949d17f7ec02c32c21112b426
rjshah00 <raj.shah8@somaiya.edu>
2021-05-02 18:32:54 +0530

    Added Video trailer funvtionality

--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,38 @@
 import React, { useEffect, useState } from "react";
 import "./Row.css";
 import axios from "./axios";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 function Row({ title, fetchUrl, isLargeRow = false }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  const opts = {
+    width: "100%",
+    height: "390",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      console.log(movie);
+      movieTrailer(movie?.name || movie?.original_title || movie?.title || "")
+        .then((url) => {
+          console.log(url);
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+          console.log(urlParams.get("v"));
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    }
+  };
 
   const base_url = "https://image.tmdb.org/t/p/original";
   useEffect(() => {
@@ -29,10 +58,12 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
                   isLargeRow ? movie.poster_path : movie.backdrop_path
                 }`}
                 alt={movie.name}
+                onClick={() => handleClick(movie)}
               />
             )
         )}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
